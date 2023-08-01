093d707eda65039c2468fd710ea2bab1a442bd37
faramarz sakhi <faramarzsakhi.49@gmail.com>
2023-08-01 20:31:33 +0330

    added observers and auto hover animation for mobile devices for products page

---
+++ src/app/common/models/itemsAxis.ts
@@ -0,0 +1,6 @@
+import { ElementRef } from "@angular/core";
+
+export class ItemAxis{
+    axisY:number=0;
+    elem:ElementRef;
+}
--- src/app/features/shop/shop/shop.component.ts
+++ src/app/features/shop/shop/shop.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { ItemAxis } from 'src/app/common/models/itemsAxis';
 
 @Component({
   selector: 'app-shop',
@@ -8,11 +9,73 @@ import { Component } from '@angular/core';
 export class ShopComponent {
   minPrice: number = 0;
   maxPrice: number = 10000;
-  ngOnInit() {
-    console.warn("shop component loaded");
+  axisArray: Array<ItemAxis> = [];
+  winY = 0;
+  windowWidth = window.innerWidth;
+  @ViewChildren("item") itemdivs: QueryList<ElementRef>;
+  itemsArray: Array<ElementRef>;
+
+
+
+
+  ngAfterViewInit() {
+
+    // observer threshold
+    let options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+          entry.target.classList.add('active')
+        } else {
+          entry.target.classList.remove('active')
+        }
+      })
+    }, options)
+    this.itemsArray = ([...this.itemdivs.toArray()])
+
+    // window width to work only in mobile size
+
+    window.onresize = () => {
+      this.windowWidth = window.innerWidth
+    }
+
+
+
+
+    this.itemsArray.forEach(item => {
+      if (this.windowWidth < 760) {
+        observer.observe(item.nativeElement);
+      }else{
+        observer.unobserve(item.nativeElement);
+      }
+
+
+    });
+
+
 
   }
-  imagesArray:Array<string>=[
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // If there were an API it would have been replaced with service and observablese and product object
+  imagesArray: Array<string> = [
     "assets/img/product/product_1.png",
     "assets/img/product/product_2.png",
     "assets/img/product/product_3.png",
